caddd6392e8f0338fc6f02357f36219ae9b08374
Shadman Afzal <shadman.afzal.7@gmail.com>
2021-10-26 17:16:52 +0530

    added leetcode #54 Spiral Matrix Solution in CPP

---
+++ Leetcode-Solutions/Matrix/spiral-matrix.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    vector<int> spiralOrder(vector<vector<int>> &matrix)
+    {
+
+        vector<int> result;
+
+        int a = 0, b = 0, c = matrix.size() - 1, d = matrix[0].size() - 1;
+
+        while (a <= c and b <= d)
+        {
+            for (int i = b; i <= d; i++)
+            {
+                result.push_back(matrix[a][i]);
+            }
+            a++;
+
+            for (int i = a; i <= c; i++)
+                result.push_back(matrix[i][d]);
+            d--;
+
+            if (a <= c)
+            {
+                for (int i = d; i >= b; i--)
+                    result.push_back(matrix[c][i]);
+                c--;
+            }
+
+            if (b <= d)
+            {
+                for (int i = c; i >= a; i--)
+                    result.push_back(matrix[i][b]);
+                b++;
+            }
+        }
+
+        return result;
+    }
+};
+
+int main()
+{
+    Solution sol = Solution();
+    vector<vector<int>> matrix{
+        {1, 2, 3},
+        {4, 5, 6},
+        {7, 8, 9},
+    };
+
+    vector<int> result = sol.spiralOrder(matrix);
+
+    for (int i : result)
+    {
+        cout << i << " ";
+    }
+}
