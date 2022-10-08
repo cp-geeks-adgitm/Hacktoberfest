c52b1c2392b66f31e816576f092b09f880d0b42e
TANISHA BANSAL <110708865+BTANISHA11@users.noreply.github.com>
2022-10-08 21:28:30 +0530

    Update Brian_Kernighan’s_algorithm.cpp

--- AlgorithmCPP/Brian_Kernighan’s_algorithm.cpp
+++ AlgorithmCPP/Brian_Kernighan’s_algorithm.cpp
@@ -35,3 +35,36 @@ int main()
 
     return 0;
 }
+#include <iostream>
+#include <bitset>
+using namespace std;
+
+// Function to count the total number of set bits in `n`
+int countSetBits(int n)
+{
+    // `count` stores the total bits set in `n`
+    int count = 0;
+
+    while (n)
+    {
+        n = n & (n - 1);    // clear the least significant bit set
+        count++;
+    }
+
+    return count;
+}
+
+//Driver code
+int main()
+{
+    int n = -1;
+
+    cout << n << " in binary is " << bitset<32>(n) << endl;
+    cout << "The total number of set bits in " << n << " is "
+         << countSetBits(n) << endl;
+
+    return 0;
+}
+//OUTPUT
+//-1 in binary is 11111111111111111111111111111111
+//The total number of set bits in -1 is 32
