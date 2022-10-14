fe7c0dc36d6f4cf86f8a985cce9ab2bdb10d3308
Nidhi Dewangan <103442972+nidhidewangan03@users.noreply.github.com>
2022-10-14 23:38:13 +0530

    Create romantointeger.py

---
+++ Leetcode-Solutions/Strings/romantointeger.py
@@ -0,0 +1,13 @@
+#roman to integer
+
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        s1={"I":1,"V":5,"X":10,"L":50,"C":100,"D":500,"M":1000}
+        s2=0
+        for i in range (len(s)):
+                if i+1<len(s) and s1[s[i]]<s1[s[i+1]]:
+                    s2-=s1[s[i]]
+                else:
+                    s2+=s1[s[i]]
+        return s2
+        
